fc5d67a28a63d3785cd528547f6c18dee722aa9d
evgr05 <evgr05@mail.ru>
2025-02-26 16:24:19 +0300

    Добавьте файлы проекта.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            await
+                Navigation.PushAsync(new PageMain());
+        }
+    }
+
+}
---
+++ MauiApp1/PageMain.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1;
+
+public partial class PageMain : ContentPage
+{
+	public PageMain()
+	{
+		InitializeComponent();
+	}
+}
