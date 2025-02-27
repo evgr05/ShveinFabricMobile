713c66857fa3267f128bcc2f9f3bea1576ade040
evgr05 <evgr05@mail.ru>
2025-02-27 14:52:29 +0300

    гойда

--- MauiApp1/PageMain.xaml.cs
+++ MauiApp1/PageMain.xaml.cs
@@ -2,8 +2,27 @@ namespace MauiApp1;
 
 public partial class PageMain : ContentPage
 {
-	public PageMain()
-	{
-		InitializeComponent();
-	}
+    public PageMain()
+    {
+        InitializeComponent();
+    }
+    void OnTextCompleted(object sender, EventArgs e)
+    {
+        loginLbl.Text = loginEntry.Text;
+    }
+
+    void OnStatusChanged(object sender, ToggledEventArgs e)
+    {
+        saveLbl.Text = saveSwitch.On ? "Данные сохранены" : "Данные не сохранены";
+    }
+
+    private void EntryCell_Completed(object sender, EventArgs e)
+    {
+        telephoneLbl.Text = telephoneEntry.Text;
+    }
+
+    private void EntryCell_Completed_1(object sender, EventArgs e)
+    {
+        danLbl.Text = danEntry.Text;
+    }
 }
